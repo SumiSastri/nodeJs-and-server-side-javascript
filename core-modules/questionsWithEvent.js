ac5d4be55969b4fe288d775919fa331c8cfd55db
Sumi Sastri <Sumitra.Sastri@gvcgroup.com>
2020-07-27 18:54:53 +0100

    adds file system

--- core-modules/questionsWithEvent.js
+++ core-modules/questionsWithEvent.js
@@ -8,14 +8,14 @@ const questions = [
 ];
 
 const answerEvents = showAnswers(questions);
-
+// log answer to console
 answerEvents.on("answer", (answer) =>
   console.log(`question answered: ${answer}`)
 );
-
+// event adds it to console to display it - additional handler with event
 answerEvents.on("complete", (answers) => {
   console.log("Thank you for your answers. ");
   console.log(answers);
 });
-
+// repeats all the events answers and completes on exit
 answerEvents.on("complete", () => process.exit());
