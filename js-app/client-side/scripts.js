21e75e330ab95710d4a70d07d00a25c44f4c3833
Sumi Sastri <Sumitra.Sastri@gvcgroup.com>
2020-07-29 18:05:06 +0100

    get and post routes working

--- js-app/client-side/scripts.js
+++ js-app/client-side/scripts.js
@@ -1,34 +1,23 @@
-// set up sockets
-const socket = io();
-// sets up jquery
+var socket = io();
 $(() => {
   $("#send").click(() => {
-    // sets input value to what user types - adds the name and message to html tags via the ids
-    let message = { name: $("#name").val(), message: $("#message").val() };
+    var message = { name: $("#name").val(), message: $("#message").val() };
     postMessage(message);
   });
-  // adds call function
   getMessages();
 });
 
-// set up socket listener
 socket.on("message", addMessage);
 
 function addMessage(message) {
-  // lists messages
   $("#messages").append(
     `<h4> ${message.name} </h4> <p> ${message.message} </p>`
   );
 }
 
 function getMessages() {
-  // gets messages from messages route
   $.get("http://localhost:5000/messages", (data) => {
-    console.log(data);
     data.forEach(addMessage);
-    // data.array.forEach((element){
-    // },this)
-    // data.map((element) =>{ addMessages });
   });
 }
 
